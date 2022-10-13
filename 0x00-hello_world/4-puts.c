24778550b22a49d5d3e75a97f0614bc0a5639ba0
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-13 01:31:39 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/**	*main prints out
-       */
+/**
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *    */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
