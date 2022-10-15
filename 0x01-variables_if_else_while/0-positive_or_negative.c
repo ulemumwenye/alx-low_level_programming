7b23a77d3ddd33e376cb5317cf1b7fa7d1b8508e
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-14 22:32:41 -0700

    task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,4 @@
-/*
-   * File: 0-positive_or_negative.c
-    * Auth: Brennan D Baraban
-     */
+
 
 #include <stdlib.h>
 #include <time.h>
@@ -25,7 +22,8 @@ printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
 else
-printf("%d is zero\n", n);					
-
+printf("%d is zero\n", n);
 return (0);
+
 }
+
