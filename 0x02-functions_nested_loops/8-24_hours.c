dab20662eb303a9dba024e1c6432c697bb52e6b7
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-18 06:19:04 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,14 @@
 
 /**
    * jack_bauer - Prints every minute of the day of
-    *              Jack Bauer, starting from 00:00 to 23:59.
+    * Jack Bauer, starting from 00:00 to 23:59.
      */
 void jack_bauer(void)
 {
 int hour, minute;
 
 for (hour = 0; hour <= 23; hour++)
-					{
+{
 for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
