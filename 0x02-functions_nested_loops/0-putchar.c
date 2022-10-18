85ec010c5d8c2aa107d60cf096b00a050e4107d5
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-18 00:46:00 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "ulemu.h"
+
+/**
+ * main - Prints "ulemu" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+ _putchar('u');
+ _putchar('l');
+ _putchar('e');
+ _putchar('m');
+ _putchar('u');
+ _putchar('\n');
+
+  return (0);
+}
