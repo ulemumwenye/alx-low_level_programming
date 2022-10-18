19e94ce1d489ddd1a02d26ec4edc7581d79e9a85
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-18 01:38:11 -0700

    task1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "0_putchar"
+#include<stdio.h>
 
 /**
    * main - Prints "_putchar" followed by a new line.
@@ -7,16 +7,16 @@
       */
 int main(void)
 {
- _putchar('_');
- _putchar('p');
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 	     
- _putchar('\n');
+_putchar('\n');
 											
 return (0);
 }
