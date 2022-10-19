da426cb82eca4b2304a9767d29a2455c394de9c2
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-19 02:01:37 -0700

    task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+  * main - tests function that prints if integer is positive or negative
+  * Return: 0
+  */
 
-[int main(void)
+int main(void)
 {
 int i;
 
@@ -13,5 +13,3 @@ i = 98;
 positive_or_negative(i);
 
 return (0);
-}
- 
