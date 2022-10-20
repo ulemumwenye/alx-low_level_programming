9ec0989fe14ddf0577b118d5caa7c6cb78de3c67
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-20 00:38:13 -0700

    task 1-2

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+/*
+ * File: main.h
+ * Auth: ulemu mwenye
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
+int _isdigit(int c);
+int _isupper(int c);
