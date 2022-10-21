2931f174c46c9d92dc998fff064fc70f6c66b480
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-20 21:14:57 -0700

    task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,8 @@
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x03-more_functions_nested_loops directory.
  */
+
+int mul(int a, int b);
 int _putchar(char c);
 int _isdigit(int c);
 int _isupper(int c);
