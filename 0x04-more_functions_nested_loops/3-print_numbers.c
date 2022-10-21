28c8e566b1f9defb888a348fa3935cae103972b6
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-20 21:37:43 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-  int num;
+int num;
 
-  for (num = 0; num <= 9; num++)
-    _putchar((num % 10) + '0');
+for (num = 0; num <= 9; num++)
+_putchar((num % 10) + '0');
 
-  _putchar('\n');
+_putchar('\n');
 }
