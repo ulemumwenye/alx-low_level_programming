71d7aa6306cf13d53703467918d213d661a1c5c8
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-31 02:12:51 -0700

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
  */
 void *_memset(void *s, int c, size_t n)
 {
-  unsigned int index;
-  unsigned char *memory = s, value = c;
+unsigned int index;
+unsigned char *memory = s, value = c;
 
-  for (index = 0; index < n; index++)
-    memory[index] = value;
+for (index = 0; index < n; index++)
+memory[index] = value;
 
-  return (memory);
+return (memory);
 }
