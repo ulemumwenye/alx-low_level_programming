de933c04f3ab67cc6365b4dc4e28162bd43e1430
ulemu mwenye <mwenyeulmu@gmail.com>
2022-10-31 02:50:47 -0700

    task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,12 +3,12 @@
 
 /**
  * File: main.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
+ * .
  */
 
 #include <stddef.h>
+
 
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
