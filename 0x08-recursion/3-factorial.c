1f26ba1fa977452da0fe0283fd3acf4d6c88bab4
ulemu mwenye <mwenyeulmu@gmail.com>
2022-11-02 01:13:59 -0700

    tasks

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,9 @@
-#include "main.h"
+include "main.h"
 
 /**
  * factorial - Returns the factorial of a given number.
  * @n: The number to find the factorial of.
+ *
  * Return: If n > 0 - the factorial of n.
  *         If n < 0 - 1 to indicate an error.
  */
@@ -19,4 +20,4 @@ return (1);
 result *= factorial(n - 1);
 
 return (result);
-} 
+}
