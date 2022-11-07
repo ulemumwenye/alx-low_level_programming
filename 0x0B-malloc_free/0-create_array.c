a08ac3030a85cbbebc95b36aa0c436365049e1e1
ulemu mwenye <mwenyeulmu@gmail.com>
2022-11-07 03:27:07 -0800

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,19 +12,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  char *array;
-  unsigned int index;
+char *array;
+unsigned int index;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-  array = malloc(sizeof(char) * size);
+array = malloc(sizeof(char) * size);
 
-  if (array == NULL)
-    return (NULL);
+if (array == NULL)
+return (NULL);
 
-  for (index = 0; index < size; index++)
-    array[index] = c;
+for (index = 0; index < size; index++)
+array[index] = c;
 
-  return (array);
+return (array);
 }
