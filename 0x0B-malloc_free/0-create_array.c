fbaa8d83e1ee6adda9d22cadc90e03525fc3d5eb
ulemu mwenye <mwenyeulmu@gmail.com>
2022-11-07 03:22:56 -0800

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,30 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * create_array - create an array of and initialize it with a specific value
- * @size: the desired size of the array
- * @c: the value with which the array should be initialized
+ * create_array - Creates an array of chars and
+ *                initializes it with a specific char.
+ * @size: The size of the array to be initialized.
+ * @c: The specific char to intialize the array with.
  *
- * Return: NULL if size is 0 or memory allocation fails,
- * otherwise 0
+ * Return: If size == 0 or the function fails - NULL.
+ *         Otherwise - a pointer to the array.
  */
 char *create_array(unsigned int size, char c)
 {
-char *array;
+  char *array;
+  unsigned int index;
 
-if (!size)
-return (NULL);
+  if (size == 0)
+    return (NULL);
 
-array = (char *) malloc(sizeof(char) * size);
+  array = malloc(sizeof(char) * size);
 
-if (array)
-{
-while (size > 0)
-array[--size] = c;
-}
-return (array);
+  if (array == NULL)
+    return (NULL);
+
+  for (index = 0; index < size; index++)
+    array[index] = c;
+
+  return (array);
 }
--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -14,37 +14,37 @@ void simple_print_buffer(char *buffer, unsigned int size)
   unsigned int i;
 
   i = 0;
-  while (i < size)
-    {
-      if (i % 10)
-        {
-	  printf(" ");
-        }
-      if (!(i % 10) && i)
-        {
-	  printf("\n");
-        }
-      printf("0x%02x", buffer[i]);
-      i++;
-    }
-  printf("\n");
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-  char *buffer;
+char *buffer;
 
-  buffer = create_array(98, 'H');
-  if  (buffer == NULL)
-    {
-      printf("failed to allocate memory\n");
-      return (1);
-    }
+buffer = create_array(98, 'H');
+if  (buffer == NULL)
+{
+printf("failed to allocate memory\n");
+return (1);
+}
 simple_print_buffer(buffer, 98);
 free(buffer);
 return (0);
