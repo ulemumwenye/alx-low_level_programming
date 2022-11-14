f5a0e075929388a5aed4e4a856acf6bed9b0e083
ulemu mwenye <mwenyeulmu@gmail.com>
2022-11-13 23:56:43 -0800

    tasks

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * Filep: p1_h
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
